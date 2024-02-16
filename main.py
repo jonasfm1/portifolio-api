f8402aedab6c2d185d9c645630947a3da07024d6
jonas_ferutcci <jonas.ferutcci@gmail.com>
2024-02-16 14:16:13 -0300

    add changing

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, json
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    personal_info = os.path.join('pages/home/personal_info.json')
+    with open(personal_info) as info:
+        data = json.load(info)
+        return data
+
+
+@app.route('/companys')
+def company():
+    personal_info = os.path.join('pages/companys/companys_names.json')
+    with open(personal_info) as info:
+        data = json.load(info)
+        return data
+
+
+app.run(debug=True)
