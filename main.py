555440a83ce348eb308c235c58be33b745309a1e
Jonas Ferutcci <jonas.ferutcci@gmail.com>
2024-11-21 01:28:58 -0500

    adjust on structure data

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, json
+from flask_cors import CORS, cross_origin
 import os
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS-HEADERS'] = 'Content-Type'
 @app.route('/')
+@cross_origin()
 def index():
     personal_info = os.path.join('pages/home/personal_info.json')
     with open(personal_info) as info:
@@ -11,6 +14,7 @@ def index():
         return data
 
 @app.route('/companys')
+@cross_origin()
 def company():
     personal_info = os.path.join('pages/companys/companys_names.json')
     with open(personal_info) as info:
@@ -18,7 +22,8 @@ def company():
         return data
 
 @app.route('/education')
-def company():
+@cross_origin()
+def education():
     personal_info = os.path.join('pages/education/education.json')
     with open(personal_info) as info:
         data = json.load(info)
